4f0c3be789ce5b4a406841a06dfe16f6c7cf1485
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-20 13:44:10 +0000

    cv median

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include <filesystem>
+
+using path = std::filesystem::path;
+
+static constexpr auto INPUT_MEDIAN = "./input-median.png";
+static constexpr auto OUTPUT_MEDIAN = "./median.png";
+
+void cv_median_filter(path in, path out){
+
+}
+
 
 int main(){
     std::cout << "hello world";
+    cv_median_filter(INPUT_MEDIAN, OUTPUT_MEDIAN);
 }
