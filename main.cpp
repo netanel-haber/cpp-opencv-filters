6d9c0de3fbb5e70cfdf6158e6e2e7006ed4fb81f
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-20 14:35:07 +0000

    read image

--- main.cpp
+++ main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <filesystem>
+#include <opencv2/opencv.hpp>
 
 using path = std::filesystem::path;
 
-static constexpr auto INPUT_MEDIAN = "./input-median.png";
+static constexpr auto INPUT_MEDIAN = "../input-median.png";
 static constexpr auto OUTPUT_MEDIAN = "./median.png";
 
-void cv_median_filter(path in, path out){
-
+cv::Mat read_grayscale(path img)
+{
+    auto image = cv::imread(img, cv::IMREAD_GRAYSCALE);
+    if (image.empty())
+    {
+        std::cout << "Could not read the image: " << img << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return image;
 }
 
+void cv_median_filter(path in, path out)
+{
+}
 
-int main(){
-    std::cout << "hello world" << std::endl;
-    cv_median_filter(INPUT_MEDIAN, OUTPUT_MEDIAN);
+void print_image_size(cv::Mat img)
+{
+    auto size = img.size();
+    std::cout << "width: " << size.width << " height: " << size.height << std::endl;
+}
+
+int main()
+{
+    auto image = read_grayscale(INPUT_MEDIAN);
+    print_image_size(image);
+    // cv_median_filter(INPUT_MEDIAN, OUTPUT_MEDIAN);
 }
