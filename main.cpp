707508b30b1321268321da35266229bf9373bd00
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-21 14:10:02 +0000

    simpler

--- main.cpp
+++ main.cpp
@@ -7,15 +7,10 @@ using path = std::filesystem::path;
 
 static constexpr auto INPUT_MEDIAN = "../input-median.png";
 static constexpr auto OUTPUT_MEDIAN = "./median.png";
-static constexpr auto KSIZE = 3;
-
-void cv_median_filter(path in, path out, int ksize = KSIZE)
-{
-    auto image = cvutils::read_grayscale(INPUT_MEDIAN);
-    cvutils::filter_to_file(image, OUTPUT_MEDIAN, cv::medianBlur, ksize);
-}
+static constexpr auto KSIZE = 9;
 
 int main()
 {
-    cv_median_filter(INPUT_MEDIAN, OUTPUT_MEDIAN);
+    auto median_input = cvutils::read_grayscale(INPUT_MEDIAN);
+    cvutils::filter_to_file(median_input, OUTPUT_MEDIAN, cv::medianBlur, KSIZE);
 }
