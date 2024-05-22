5ccccd4bd99f26360047df20c8c26209bf23fb90
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-22 20:16:52 +0300

    download opencv cross platform

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <filesystem>
 #include <opencv2/opencv.hpp>
 #include "./utils.hpp"
 
@@ -7,13 +5,15 @@ auto KSIZE = 5;
 
 int main()
 {
+    using namespace cv;
+
     auto median_input = cvutils::read_grayscale("../input-median.png");
     auto dst = median_input.clone();
-    cv::medianBlur(median_input, dst, KSIZE);
-    cv::imwrite("./median.png", dst);
+    medianBlur(median_input, dst, KSIZE);
+    imwrite("./median.png", dst);
 
     auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
     dst = fabio_input.clone();
-    cv::GaussianBlur(fabio_input, dst, cv::Size{KSIZE, KSIZE}, 2);
-    cv::imwrite("./fabio.png", dst);
+    GaussianBlur(fabio_input, dst, Size{KSIZE, KSIZE}, 0);
+    imwrite("./fabio.png", dst);
 }
