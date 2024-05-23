5d0b1f8331ed3d2aded799b79000554b42860752
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-23 17:59:57 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,23 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include "./utils.hpp"
 
-auto KSIZE = 5;
-
 int main()
 {
-    using namespace cv;
-
-    auto median_input = cvutils::read_grayscale("../input-median.png");
-    auto dst = median_input.clone();
-    medianBlur(median_input, dst, KSIZE);
-    imwrite("./median.png", dst);
-
-    auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
-    dst = fabio_input.clone();
-    GaussianBlur(fabio_input, dst, Size{KSIZE, KSIZE}, 0);
-    imwrite("./gaussian.png", dst);
-
-    dst = fabio_input.clone();
-    Laplacian(fabio_input, dst, -1, 5);
-    imwrite("./laplacian.png", dst);
+    cvutils::cv_filters("./median.png", "./gaussian.png", "./laplacian.png");
 }
--- utils.hpp
+++ utils.hpp
@@ -3,11 +3,14 @@
 #include <opencv2/opencv.hpp>
 #include <filesystem>
 
+auto static constexpr KSIZE = 5;
+
 namespace cvutils
 {
-    cv::Mat read_grayscale(std::filesystem::path img)
+    using namespace cv;
+    Mat read_grayscale(std::filesystem::path img)
     {
-        auto image = imread(img, cv::IMREAD_GRAYSCALE);
+        auto image = imread(img, IMREAD_GRAYSCALE);
         if (image.empty())
         {
             std::cout << "Could not read the image: " << img << std::endl;
@@ -15,5 +18,23 @@ namespace cvutils
         }
         assert(image.channels() == 1);
         return image;
+    }
+
+    void cv_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian)
+    {
+
+        auto median_input = cvutils::read_grayscale("../input-median.png");
+        auto dst = median_input.clone();
+        medianBlur(median_input, dst, KSIZE);
+        imwrite(outputMedian, dst);
+
+        auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
+        dst = fabio_input.clone();
+        GaussianBlur(fabio_input, dst, Size{KSIZE, KSIZE}, 0);
+        imwrite(outputGaussian, dst);
+
+        dst = fabio_input.clone();
+        Laplacian(fabio_input, dst, -1, 5);
+        imwrite(outputLaplacian, dst);
     }
 }
