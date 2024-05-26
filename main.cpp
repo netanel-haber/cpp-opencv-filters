868b940fc0376d385f866b6c1705f68fe5e8e89f
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-26 14:49:21 +0300

    cleanup

--- main.cpp
+++ main.cpp
@@ -2,9 +2,35 @@
 
 #include "opencv2/highgui/highgui.hpp"
 
+auto static constexpr KSIZE = 11;
+
+void cv_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian)
+{
+    auto median_input = cvutils::read_grayscale("../input-median.png");
+    auto dst = median_input.clone();
+    medianBlur(median_input, dst, KSIZE);
+    imwrite(outputMedian, dst);
+
+    auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
+    dst = fabio_input.clone();
+    GaussianBlur(fabio_input, dst, cv::Size{ KSIZE, KSIZE }, 0);
+    imwrite(outputGaussian, dst);
+
+    dst = fabio_input.clone();
+    Laplacian(fabio_input, dst, -1, 5);
+    imwrite(outputLaplacian, dst);
+}
+
+void our_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian)
+{
+    auto median_input = cvutils::read_grayscale("../input-median.png");
+    auto dst = cvutils::median_blur(median_input, KSIZE);
+    cvutils::show_image(dst);
+}
+
 int main()
 {
     cv::namedWindow(cvutils::WINDOW_NAME);
-    cvutils::cv_filters("./median.png", "./gaussian.png", "./laplacian.png");
-    cvutils::our_filters("./our_median.png", "./our_gaussian.png", "./our_laplacian.png");
+    cv_filters("./median.png", "./gaussian.png", "./laplacian.png");
+    our_filters("./our_median.png", "./our_gaussian.png", "./our_laplacian.png");
 }
--- utils.hpp
+++ utils.hpp
@@ -7,88 +7,62 @@
 #include <stddef.h>
 #include <iostream>
 
-auto static constexpr KSIZE = 11;
 
 namespace cvutils
 {
-    using namespace cv;
+using namespace cv;
 
-    auto constexpr WINDOW_NAME = "Display Image";
-    void show_image(const Mat &img)
+auto constexpr WINDOW_NAME = "Display Image";
+void show_image(const Mat& img)
+{
+    imshow(WINDOW_NAME, img);
+    waitKey(3000);
+}
+
+Mat read_grayscale(std::filesystem::path img)
+{
+    auto image = imread(img, IMREAD_GRAYSCALE);
+    if (image.empty())
     {
-        imshow(WINDOW_NAME, img);
-        waitKey(3000);
+        std::cout << "Could not read the image: " << img << std::endl;
+        exit(EXIT_FAILURE);
     }
+    assert(image.channels() == 1);
+    return image;
+}
 
-    Mat read_grayscale(std::filesystem::path img)
+Mat pad(const Mat& src, int ksize)
+{
+    auto border = (ksize - 1) / 2;
+    Mat padded;
+    copyMakeBorder(src, padded, border, border, border, border, BORDER_CONSTANT, 0);
+    assert((src.rows + (border * 2)) == padded.rows);
+    assert((src.cols + (border * 2)) == padded.cols);
+
+    return padded;
+}
+
+Mat median_blur(const Mat& src, int ksize)
+{
+    Mat dst = src.clone();
+    Mat padded = pad(src, ksize);
+
+    auto padding = (ksize - 1) / 2;
+    for (auto r = padding; r < padded.rows - padding; r++)
     {
-        auto image = imread(img, IMREAD_GRAYSCALE);
-        if (image.empty())
+        for (auto c = padding; c < padded.cols - padding; c++)
         {
-            std::cout << "Could not read the image: " << img << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        assert(image.channels() == 1);
-        return image;
-    }
-
-    void cv_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian)
-    {
-
-        auto median_input = cvutils::read_grayscale("../input-median.png");
-        auto dst = median_input.clone();
-        medianBlur(median_input, dst, KSIZE);
-        imwrite(outputMedian, dst);
-
-        auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
-        dst = fabio_input.clone();
-        GaussianBlur(fabio_input, dst, Size{KSIZE, KSIZE}, 0);
-        imwrite(outputGaussian, dst);
-
-        dst = fabio_input.clone();
-        Laplacian(fabio_input, dst, -1, 5);
-        imwrite(outputLaplacian, dst);
-    }
-
-    Mat pad(const Mat &src, int ksize)
-    {
-        auto border = (ksize - 1) / 2;
-        Mat padded;
-        copyMakeBorder(src, padded, border, border, border, border, BORDER_CONSTANT, 0);
-        assert((src.rows + (border * 2)) == padded.rows);
-        assert((src.cols + (border * 2)) == padded.cols);
-
-        return padded;
-    }
-
-    Mat median_blur(const Mat &src, int ksize)
-    {
-        Mat dst = src.clone();
-        Mat padded = pad(src, ksize);
-
-        auto padding = (ksize - 1) / 2;
-        for (auto r = padding; r < padded.rows - padding; r++)
-        {
-            for (auto c = padding; c < padded.cols - padding; c++)
+            int sum = 0;
+            for (auto i = r - padding; i < r + padding + 1; i++)
             {
-                int sum = 0;
-                for (auto i = r - padding; i < r + padding + 1; i++)
+                for (auto j = c - padding; j < c + padding + 1; j++)
                 {
-                    for (auto j = c - padding; j < c + padding + 1; j++)
-                    {
-                        sum += padded.at<uint8_t>(i, j);
-                    }
+                    sum += padded.at<uint8_t>(i, j);
                 }
-                dst.at<uint8_t>(r - padding, c - padding) = sum / (ksize * ksize);
             }
+            dst.at<uint8_t>(r - padding, c - padding) = sum / (ksize * ksize);
         }
-        return dst;
     }
-
-    void our_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian)
-    {
-        auto median_input = cvutils::read_grayscale("../input-median.png");
-        Mat dst = median_blur(median_input, KSIZE);
-        show_image(dst);
-    }
+    return dst;
+}
 }
