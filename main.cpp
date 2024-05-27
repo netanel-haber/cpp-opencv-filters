c16168ac4cfe1d1a5c9802b37ae38e5a1268bb03
Netanel Haber <netanel.t.haber@gmail.com>
2024-05-27 15:16:02 +0300

    simplify build

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ static_assert(KSIZE % 2 == 1);
 
 void cv_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian) {
   cv::Mat dst;
-  auto median_input = cvutils::read_grayscale("../input-median.png");
+  auto median_input = cvutils::read_grayscale("input-median.png");
   dst = median_input.clone();
   medianBlur(median_input, dst, KSIZE);
   imwrite(outputMedian, dst);
 
-  auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
+  auto fabio_input = cvutils::read_grayscale("input-fabio.png");
   dst = fabio_input.clone();
   GaussianBlur(fabio_input, dst, cv::Size {KSIZE, KSIZE}, 0);
   imwrite(outputGaussian, dst);
@@ -23,16 +23,16 @@ void cv_filters(std::string outputMedian, std::string outputGaussian, std::strin
 
 void our_filters(std::string outputMedian, std::string outputGaussian, std::string outputLaplacian) {
   cv::Mat dst;
-  auto median_input = cvutils::read_grayscale("../input-median.png");
+  auto median_input = cvutils::read_grayscale("input-median.png");
   dst = cvutils::median_blur(median_input, KSIZE);
   imwrite(outputMedian, dst);
 
-  auto fabio_input = cvutils::read_grayscale("../input-fabio.png");
+  auto fabio_input = cvutils::read_grayscale("input-fabio.png");
   dst = cvutils::gaussian_blur(fabio_input, KSIZE);
   imwrite(outputGaussian, dst);
 }
 
 int main() {
-  cv_filters("./median.png", "./gaussian.png", "./laplacian.png");
-  our_filters("./our_median.png", "./our_gaussian.png", "./our_laplacian.png");
+  cv_filters("./build/median.png", "./build/gaussian.png", "./build/laplacian.png");
+  our_filters("./build/our_median.png", "./build/our_gaussian.png", "./build/our_laplacian.png");
 }
